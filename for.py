9dfbef52c70f54ae22fa778a3d901d23ec40c665
Denis Kochmashev <d.kochmashev@gmail.com>
2024-05-21 10:51:00 +0500

    Create for.py
    Самостоятельная работа по уроку "Цикл for. Элементы списка. Полезные функции в цикле"

---
+++ for.py
@@ -0,0 +1,9 @@
+cars = ["BMW", "MB", "LADA", "KIA", "HONDA"]
+for car in cars:
+    print('Я езжу на автомобиле марки', car)
+
+cars_count = 0
+for car in cars:
+    cars_count += 10
+
+print(cars_count)   # В задании нет, но зачем тогда вообще cars_count?
