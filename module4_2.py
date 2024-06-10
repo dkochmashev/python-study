ce324fad8fec863a8b855eb94b9d53dae20b21de
Denis Kochmashev <d.kochmashev@gmail.com>
2024-06-10 20:36:27 +0500

    Update module4_2.py
    Добавил комментарии

--- module4_2.py
+++ module4_2.py
@@ -1,4 +1,7 @@
+# Создайте новую функцию def test_function
 def test_function():
+    # Создайте другую функцию внутри функции inner_function,
+    # функция должна печатать значение "Я в области видимости функции test_function"
     def inner_function():
         print("Я в области видимости функции test_function")
     inner_function()
@@ -6,6 +9,8 @@ def test_function():
 test_function()
 
 try:
+    # Попробуйте вызывать inner_function вне функции test_function
+    # и посмотрите на результат выполнения программы
     inner_function()    # Сформирует исключение NameError с описанием "name 'inner_function' is not defined"
 except Exception as e:
     print(type(e), e)
