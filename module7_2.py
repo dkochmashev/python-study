8a382576b1de43f9000bf75ae29c31df4c008760
Denis Kochmashev <d.kochmashev@gmail.com>
2024-07-11 09:43:29 +0500

    Update module7_2.py
    Устранил зависимость от типа операционной системы

--- module7_2.py
+++ module7_2.py
@@ -1,3 +1,6 @@
+import builtins
+import platform
+
 # Создайте функцию custom_write(file_name, strings), которая принимает аргументы
 #    file_name - название файла для записи,
 #    strings - список строк для записи.
@@ -6,6 +9,29 @@
 # 2. Возвращать словарь strings_positions, где ключом будет кортеж (<номер строки>, <байт начала строки>),
 #    а значением - записываемая строка. Для получения номера байта начала строки используйте метод tell()
 #    перед записью.
+
+class OSIndependentLineWriter:
+    '''
+    Класс для записи строк в файл с использованием CRLF для переноса строк
+    независимо от используемой операционной системы.
+    '''
+    NEWLINE = '\n' if platform.system() == 'Windows' else '\r\n'
+
+    def __init__(self, file_handle):
+        self.__file_handle = file_handle
+
+    def write(self, string):
+        return self.__file_handle.write(f'{string}{self.NEWLINE}')
+
+    def tell(self):
+        return self.__file_handle.tell()
+
+    def close(self):
+        return self.__file_handle.close()
+
+def open(file_name, *args, **kwargs):
+    return OSIndependentLineWriter(builtins.open(file_name, *args, **kwargs))
+
 def custom_write(file_name, strings):
     result = dict()
 
@@ -16,7 +42,7 @@ def custom_write(file_name, strings):
 
     for line_number, string in enumerate(strings):
         result[(line_number + 1, file.tell())] = string
-        file.write(f'{string}\n')
+        file.write(string)
     file.close()
 
     return result
