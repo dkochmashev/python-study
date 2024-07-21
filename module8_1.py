a1b69334d92a53f28a761a05409b59bf1d699e8f
Denis Kochmashev <d.kochmashev@gmail.com>
2024-07-21 14:28:11 +0500

    Create module8_1.py
    Задание "Программистам всё можно"

---
+++ module8_1.py
@@ -0,0 +1,15 @@
+# add_everything_up(a, b) принимает a и b, которые могут быть как числами(int, float),
+# так и строками(str).
+# TypeError - когда a и b окажутся разными типами (числом и строкой), то возвращать
+# строковое представление этих двух данных вместе (в том же порядке). Во всех остальных
+# случаях выполнять стандартные действия.
+
+def add_everything_up(a, b):
+    try:
+        return a + b
+    except TypeError:
+        return str(a) + str(b)
+
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
