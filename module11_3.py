4e1efc5ce2c56d92cb86bf568141c1128b37d0b1
Denis Kochmashev <d.kochmashev@gmail.com>
2024-08-28 09:55:36 +0500

    Update module11_3.py
    Добавил обработку ошибок при отсутствии метода __init__ в исследуемом объекте

--- module11_3.py
+++ module11_3.py
@@ -1,16 +1,36 @@
 import inspect
 
+
 class Inspectable:
     def __init__(self):
         self.property = 42
 
+
 def introspection_info(obj):
+    init_source = None
+
+    try:
+        init_source = inspect.getsource(obj.__init__)
+    except TypeError:
+        pass
+
     return {
-        'type' : obj.__class__.__name__,
-        'methods' : dir(obj),
-        'module' : obj.__class__.__module__,
-        'init_source' : inspect.getsource(obj.__init__)
+        'type': obj.__class__.__name__,
+        'methods': dir(obj),
+        'module': obj.__class__.__module__,
+        'init_source': init_source
     }
+
+
+def test_func():
+    return 1
+
+
+class_info = introspection_info(42)
+print(class_info)
+
+class_info = introspection_info(test_func)
+print(class_info)
 
 class_info = introspection_info(Inspectable())
 print(class_info)
