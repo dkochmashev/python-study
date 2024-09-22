21b0a323ba2f4be57025d3ae9916f8e6207db0ca
Denis Kochmashev <d.kochmashev@gmail.com>
2024-09-22 19:04:54 +0500

    Задача "Регистрация покупателей"

--- crud_functions.py
+++ crud_functions.py
@@ -1,3 +1,4 @@
+import sqlite3
 import sqlite3 as db
 
 
@@ -27,6 +28,7 @@ class Storage:
             'image': 'https://46tv.ru/uploads/posts/2020-09/1599749657_1595606483_9-p-samogonshchiki-film-55.jpg'
         }
     ]
+    INITIAL_BALANCE = 1000
 
     def __init__(self):
         self.db_connection = None
@@ -49,26 +51,36 @@ class Storage:
         if self.db_connection is None:
             return
 
-        if self.__table_exists__('Products'):
-            return
+        if not self.__table_exists__('Products'):
+            self.db_connection.execute('''
+                CREATE TABLE IF NOT EXISTS "Products" (
+                    "id"            INTEGER NOT NULL,
+                    "title"         TEXT NOT NULL UNIQUE,
+                    "description"   TEXT,
+                    "price"         INTEGER NOT NULL,
+                    "image"         TEXT,
+                    PRIMARY KEY ("id" AUTOINCREMENT)
+                )
+            ''')
 
-        self.db_connection.execute('''
-            CREATE TABLE IF NOT EXISTS "Products" (
-                "id"            INTEGER NOT NULL,
-                "title"         TEXT NOT NULL,
-                "description"   TEXT,
-                "price"         INTEGER NOT NULL,
-                "image"         TEXT,
-                PRIMARY KEY ("id" AUTOINCREMENT)
-            )
-        ''')
+            self.db_connection.executemany('''
+                INSERT INTO Products (title, description, price, image)
+                    VALUES (?, ?, ?, ?)
+            ''', (tuple(product[field] for field in ('title', 'description', 'price', 'image'))
+                  for product in self.INITIAL_PRODUCTS)
+                                           )
+        if not self.__table_exists__('Users'):
+            self.db_connection.execute('''
+                CREATE TABLE IF NOT EXISTS "Users" (
+                    "id"            INTEGER NOT NULL,
+                    "username"      TEXT NOT NULL UNIQUE,
+                    "email"         TEXT NOT NULL UNIQUE,
+                    "age"           INTEGER NOT NULL,
+                    "balance"       INTEGER NOT NULL,
+                    PRIMARY KEY ("id" AUTOINCREMENT)
+                )
+            ''')
 
-        self.db_connection.executemany('''
-            INSERT INTO Products (title, description, price, image)
-                VALUES (?, ?, ?, ?)
-        ''', (tuple(product[field] for field in ('title', 'description', 'price', 'image'))
-              for product in self.INITIAL_PRODUCTS)
-                                       )
         self.db_connection.commit()
 
     def __del__(self):
@@ -76,6 +88,39 @@ class Storage:
             return
 
         self.db_connection.close()
+        self.db_connection = None
+
+    def add_user(self, username, email, age):
+        if self.db_connection is None:
+            return
+
+        try:
+            result = self.db_connection.execute('''
+                INSERT INTO Users (username, email, age, balance)
+                    VALUES (?, ?, ?, ?)
+            ''', (username, email, age, self.INITIAL_BALANCE))
+
+            self.db_connection.commit()
+        except sqlite3.Error:
+            return -1
+
+        return 1
+
+    def is_included(self, username=None, email=None):
+        if self.db_connection is None:
+            return
+
+        if username is not None:
+            return self.db_connection.execute('''
+                SELECT COUNT(*) FROM Users
+                    WHERE username = ?
+            ''', (username,)).fetchone()[0] > 0
+
+        if email is not None:
+            return self.db_connection.execute('''
+                SELECT COUNT(*) FROM Users
+                    WHERE email = ?
+            ''', (email,)).fetchone()[0] > 0
 
     def get_all_products(self):
         return (
